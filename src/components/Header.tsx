f8643f59d3b79b3b80489933986c070b2b87cb72
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 10:11:03 +0100

    style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,55 @@
-import { Box, Button, Flex, Heading, Icon } from '@chakra-ui/react'
-import { HambergerMenu } from 'iconsax-react'
-import React from 'react'
-
+import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import {
+  Facebook,
+  Google,
+  HambergerMenu,
+  Instagram,
+  Twitch,
+  Youtube,
+} from "iconsax-react";
+import Link from "next/link";
+import React from "react";
+import Nav from "./Nav";
+import { motion } from "framer-motion";
 const Header = () => {
   return (
-   <Box>
-    <Flex justify={"space-around"}>
-       <Flex align={"center"}>
-       <Icon as={HambergerMenu} />
-        <Heading className='typography'>.News</Heading>
-       </Flex>
-       <Button>Contact Us</Button>
-    </Flex>
-   </Box>
-  )
-}
+    <Box px={10}>
+      <Flex justify={"space-between"} p={5} align="center">
+        <Flex align={"center"} gap={5}>
+          <Icon as={HambergerMenu} fontSize={40} cursor={"pointer"} />
+          <Link href="/">
+            <Heading as={motion.h1} layout className="typography">
+              .News Blog
+            </Heading>
+          </Link>
+        </Flex>
+        {/* <Button>Contact Us</Button> */}
+        <Flex gap={5}>
+          <Link href={""} title="Follow us on :::::::">
+            {" "}
+            <Icon variant="Bold" as={Facebook} />
+          </Link>
+          <Link href={""} title="Follow us on :::::::">
+            {" "}
+            <Icon variant="Bold" as={Instagram} />
+          </Link>
+          <Link href={""} title="Follow us on :::::::">
+            {" "}
+            <Icon variant="Bold" as={Youtube} />
+          </Link>
+          <Link href={""} title="Follow us on :::::::">
+            {" "}
+            <Icon variant="Bold" as={Twitch} />
+          </Link>
+          <Link href={""} title="Follow us on :::::::">
+            {" "}
+            <Icon variant="Bold" as={Google} />
+          </Link>
+        </Flex>
+      </Flex>
+      <Nav />
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
