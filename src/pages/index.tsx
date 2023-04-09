e9ac0b452aa37bc2488799cef74ee4fab6f2f701
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 10:28:52 +0100

    style: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import { Header } from "@/components";
 import { PostProvider } from "@/context/postContext";
+import { Spinner } from "@chakra-ui/react";
 import _ from "lodash";
 import Head from "next/head";
 export default function Home() {
   return (
-   <PostProvider>
-    <Head>
-      <title>.Newz | Home</title>
-    </Head>
-    <>
-    <Header />
-    </>
-   </PostProvider>
+    <PostProvider>
+      <Head>
+        <title>.Newz | Home</title>
+      </Head>
+      <Spinner mx={10} />
+    </PostProvider>
   );
 }
