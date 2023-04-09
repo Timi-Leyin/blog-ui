b479b8997f2947f467f0301b0e0dbb13f0ecabdd
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 13:57:11 +0100

    feat: added post context provider wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import type { AppProps } from "next/app";
 import { theme } from "../configs/theme";
 import { NotificationsProvider } from "reapop";
 import { Header } from "@/components";
+import { PostProvider } from "@/context/postContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NotificationsProvider>
         <ChakraProvider theme={theme}>
+        <PostProvider>
           <Header />
           <Component {...pageProps} />
+        </PostProvider>
         </ChakraProvider>
       </NotificationsProvider>
     </>
