a1a7ec000b8f2d7f45cc9c2562fa12a07eeed72f
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 13:57:33 +0100

    feat: wait for posts loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
-import { PostCard, PostCardSkeleton, PostGridLoader } from "@/components";
-import { PostProvider } from "@/context/postContext";
+import {
+  ErrorWrapper,
+  PostCard,
+  PostCardSkeleton,
+  PostGrid,
+  PostGridLoader,
+} from "@/components";
+import postContext, { PostProvider } from "@/context/postContext";
 import { Box, Flex, Grid, GridItem, Spinner } from "@chakra-ui/react";
 import _ from "lodash";
 import Head from "next/head";
+import { useContext } from "react";
 export default function Home() {
+  const posts: any = useContext(postContext);
+  // console.log(posts);
   return (
-    <PostProvider>
+    // <PostProvider>
+    <>
       <Head>
         <title>.Newz | Home</title>
       </Head>
       <Box px={10} pt={"20vh"}>
-       <PostGridLoader />
+        {posts.loading ? (
+          <PostGridLoader />
+        ) : (
+          <ErrorWrapper error={posts.error}>
+            <PostGrid posts={posts.data} />
+          </ErrorWrapper>
+        )}
       </Box>
-    </PostProvider>
+    </>
+    // </PostProvider>
   );
 }
