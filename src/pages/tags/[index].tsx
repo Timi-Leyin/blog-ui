45a445f042124958b2c1aab4dfd2bb6ba98ad1ce
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 11:45:28 +0100

    feat: add title to tags page

--- src/pages/tags/[index].tsx
+++ src/pages/tags/[index].tsx
@@ -1,6 +1,7 @@
 import { GetRequest } from '@/utils/request'
 import { Box, Center, Heading, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import Head from 'next/head'
+import React, { Fragment } from 'react'
 
 export async function getServerSideProps(context){
 const {status, data, statusText } = await GetRequest("/posts")
@@ -13,7 +14,11 @@ const {status, data, statusText } = await GetRequest("/posts")
 
 const TagsPage = ({query}) => {
   return (
-  <Box px={10}>
+ <Fragment>
+  <Head>
+    <title>.News | Tags - {query}</title>
+  </Head>
+   <Box pt={"13%"} px={10}>
    <Center color="wheat" bgImage={"linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6)), url(/thumbnail.webp)"} p={10} flexDirection={"column"}>
    <Text>Tags</Text>
     <Heading textTransform={"uppercase"} className="typography" fontSize={"5xl"} mt={2}>{query}</Heading>
@@ -24,6 +29,7 @@ const TagsPage = ({query}) => {
     <Text fontWeight={"bold"}>Currently not Availble</Text>
    </Center>
   </Box>
+ </Fragment>
   )
 }
 
