5a9ff9c8cc7e76f8b6cfee23d480ab7cbfbc9679
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 13:56:55 +0100

    feat: errorWrapper props

--- src/interface/components.d.ts
+++ src/interface/components.d.ts
@@ -1,5 +1,17 @@
+import { ReactNode } from "react"
+
 interface PostCardProps {
     thumbnail?:boolean,
+    post:any,
     fontSize?:string,
     textSize?:string
+}
+
+interface ErrorWrapperProps {
+    error?:boolean,
+    children:ReactNode | any
+}
+
+interface PostGridProps {
+    posts:any
 }
