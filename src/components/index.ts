194805c1442147740d3494e29bcdd737acc9be6a
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 11:43:04 +0100

    feat : export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export {default as Header} from "./Header"
+export {default as PostCard} from "./PostCard"
+export {default as PostCardSkeleton} from "./PostCardSkeleton"
+export {default as PostGridLoader} from "./PostGridLoader"
