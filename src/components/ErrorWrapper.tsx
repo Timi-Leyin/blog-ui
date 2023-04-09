5e7c232e5902b930cd3b6415d179837cde2a4035
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-04-09 13:55:43 +0100

    feat: error fallback

---
+++ src/components/ErrorWrapper.tsx
@@ -0,0 +1,13 @@
+import { ErrorWrapperProps } from "@/interface/components";
+import { Box, Center } from "@chakra-ui/react";
+import React from "react";
+
+const ErrorWrapper = ({ error = false, children }: ErrorWrapperProps) => {
+  return error ? <Box>
+    <Center w="full" py={"20vh"} fontWeight="bold" h="full">
+        404 | An Error Occured
+    </Center>
+  </Box> : children;
+};
+
+export default ErrorWrapper;
