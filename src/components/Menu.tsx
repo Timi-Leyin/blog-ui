7023057b11c5de36fb6fd60d0f408efd5dfb473e
Original Timi <timileyinoyelekan11@gmail.com>
2023-05-06 11:15:31 +0000

    style: minor fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,37 +4,37 @@ import Link from "next/link";
 import React from "react";
 import PostCardSkeleton from "./PostCardSkeleton";
 
-const Menu = () => {
-  return (
-    <Box zIndex={20} position={"fixed"} h="full" w="full" bg={"#eee"}>
-      <Heading>
-        <Flex cursor={"pointer"} gap={5} p={10}>
-          <span>&times;</span>
-          Close
-        </Flex>
-      </Heading>
-      <Flex justifyContent={"space-around"}>
-        <Link href={""}>About us</Link>
-       <Box>
-        <Text fontWeight="bold">Follow us</Text>
-       <Flex my={4} gap={4}>
-        <Link href={""}>Twitter</Link>
-        <Link href={""}>Facebook</Link>
-        <Link href={""}>Instagram</Link>
-        </Flex>
-       </Box>
+const Menu = ({isOpen}:{isOpen:boolean}) => {
+  return isOpen && (
+    <Box backdropFilter={"blur(10px)"} zIndex={20} position={"fixed"} h="full" w="full" bg={"rgba(255,255,255,.5)"}>
+    <Heading>
+      <Flex cursor={"pointer"} gap={5} p={10}>
+        <span>&times;</span>
+        Close
       </Flex>
-      <Flex align={"center"} maxW="500px" mx="auto" gap={2} py={1}>
-        <Input h="50px" placeholder="Search Anything" />
-        {/* <SearchNormal /> */}
+    </Heading>
+    <Flex align={"center"} justifyContent={"space-around"}>
+      <Link href={""}>About us</Link>
+     <Box>
+      <Text fontWeight="bold">Follow us</Text>
+     <Flex my={2} gap={4}>
+      <Link href={""}>Twitter</Link>
+      <Link href={""}>Facebook</Link>
+      <Link href={""}>Instagram</Link>
       </Flex>
+     </Box>
+    </Flex>
+    <Flex align={"center"} maxW="500px" mx="auto" gap={2} py={1}>
+      <Input border="1px solid #222" h="50px" type="search" name="search" placeholder="Search Anything" />
+      {/* <SearchNormal /> */}
+    </Flex>
 
 
-      <Box p={10}>
-            <PostCardSkeleton />
-      </Box>
+    <Box p={10}>
+          <PostCardSkeleton />
     </Box>
-  );
+  </Box>
+  )
 };
 
 export default Menu;
